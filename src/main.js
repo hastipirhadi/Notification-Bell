e6fcb6329b27ef5560229129edaa75e0d3ecd508
hasti_phd <hpirhadi@yahoo.com>
2026-02-27 17:43:46 +0330

    finish-project

--- src/main.js
+++ src/main.js
@@ -4,9 +4,53 @@ const countEl = document.getElementById("count");
 const message1El = document.getElementById("message1");
 const message2El = document.getElementById("message2");
 const message3El = document.getElementById("message3");
-const message4El = document.getElementById("message4");
-const cleanEl=document.getElementById("clear");
-notificationIconEl.addEventListener("click",()=>{
-notificationListEl.classList.toggle("show");
+const cleanEl = document.getElementById("clear");
+let cont;
 
-})
+notificationIconEl.addEventListener("click", () => {
+  notificationListEl.classList.toggle("show");
+});
+
+message1El.addEventListener("click", () => {
+  message1El.classList.add("border");
+  cont = countEl.innerText;
+  if (cont != 0) {
+    cont--;
+    countEl.innerText = cont;
+    if (cont == 0) {
+      countEl.innerText == "";
+    }
+  }
+});
+message2El.addEventListener("click", () => {
+  message2El.classList.add("border");
+  cont = countEl.innerText;
+  if (cont != 0) {
+    cont--;
+    countEl.innerText = cont;
+    if (cont == 0) {
+      countEl.innerText == "";
+    }
+  }
+});
+message3El.addEventListener("click", () => {
+  message3El.classList.add("border");
+  cont = countEl.innerText;
+  if (cont != 0) {
+    cont--;
+    countEl.innerText = cont;
+    if (cont == 0) {
+      countEl.innerText == "";
+    }
+  }
+});
+
+cleanEl.addEventListener("click", () => {
+  message1El.classList.add("border");
+  message2El.classList.add("border");
+  message3El.classList.add("border");
+});
+
+cleanEl.addEventListener("click", () => {
+  countEl.classList.add("!hidden");
+});
